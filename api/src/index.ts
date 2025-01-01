b04231477822ff2519ae702a528510c5ee7add38
theDone <done.muzaffer5@gmail.com>
2025-01-01 17:38:07 +0300

    json parsing middleware

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,7 +1,12 @@
-import express from "express";
+import express, { urlencoded } from "express";
+import { json } from "express"; // import json from express
 import productsRouter from "./routers/products/products";
 const app = express();
 const port = 5000;
+
+app.use(urlencoded({extended:false})) // POST ile gelen verileri bir nesneye dönüştürür ve ona request.body üzerinden ulaşmamızı sağlar. false değeri ile sadece string ve array değerlerini alırız. (Bu yüzden urlencoded middleware'ini kullanmamız gerekiyor.)
+app.use(json()); // use json middleware and request body parser (Bu yüzden json middleware'ini kullanmamız gerekiyor.)
+
 
 app.get("/", (req, res) => {
   res.send("Hello World");
