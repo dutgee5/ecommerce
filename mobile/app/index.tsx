2152a8fbbaf5ac8b0f0076dffac9d7902bad1512
theDone <done.muzaffer5@gmail.com>
2025-01-12 08:39:30 +0300

    Product Item gluestack

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -3,18 +3,14 @@ import products from "../assets/products.json"; // dummy data
 import ProductListItem from "../components/ProductListItem";
 import { Button, ButtonText } from "@/components/ui/button";
 
-
 export default function HomeScreen() {
-
-  return(
-    <Button variant="outline">
-      <ButtonText>Press me</ButtonText>
-    </Button>
-  )
-  // return (
-  //   <FlatList
-  //     data={products}
-  //     renderItem={({ item }) => <ProductListItem product={item} />}
-  //   ></FlatList>
-  // );
+  return (
+    <FlatList
+      data={products}
+      numColumns={2}
+      contentContainerClassName="gap-2"
+      columnWrapperClassName="gap-2"
+      renderItem={({ item }) => <ProductListItem product={item} />}
+    ></FlatList>
+  );
 }
