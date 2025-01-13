d1c0c91471ac972a05210dd2abad8cc12602a66d
theDone <done.muzaffer5@gmail.com>
2025-01-13 13:30:50 +0300

    Product detail screen

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -1,7 +1,6 @@
-import { View, Text, FlatList } from "react-native";
+import { FlatList } from "react-native";
 import products from "../assets/products.json"; // dummy data
 import ProductListItem from "../components/ProductListItem";
-import { Button, ButtonText } from "@/components/ui/button";
 
 export default function HomeScreen() {
   return (
