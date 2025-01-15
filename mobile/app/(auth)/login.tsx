3f621d20ca67a82864dffc6a6473891bf56d1377
theDone <done.muzaffer5@gmail.com>
2025-01-15 06:59:28 +0300

    Authentication

--- mobile/app/(auth)/login.tsx
+++ mobile/app/(auth)/login.tsx
@@ -7,38 +7,92 @@ import { EyeIcon, EyeOffIcon } from "lucide-react-native";
 import { Text } from "@/components/ui/text";
 import { useState } from "react";
 import { HStack } from "@/components/ui/hstack";
+import { useMutation } from "@tanstack/react-query";
+import { login, signup } from "@/api/auth";
+import { useAuth } from "@/store/autStore";
+import { Redirect } from "expo-router";
 
 export default function LoginScreen() {
   const [showPassword, setShowPassword] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const setUser = useAuth((state) => state.setUser);
+  const setToken = useAuth((state) => state.setToken);
+  const isLoggedIn = useAuth((state) => !!state.user);
+
+  const loginMutation = useMutation({
+    mutationFn: () => login(email, password),
+    onSuccess: (data) => {
+      console.log("success", data);
+      if (data.token && data.user) {
+        setUser(data.user);
+        setToken(data.token);
+      }
+    },
+    onError: () => {
+      console.log("error");
+    },
+  });
+
+  const sigupMutation = useMutation({
+    mutationFn: () => signup(email, password),
+    onSuccess: (data) => {
+      console.log("success signup:", data);
+      if (data.token && data.user) {
+        setUser(data.user);
+        setToken(data.token);
+      }
+    },
+    onError: (error) => {
+      console.log("error:", error);
+    },
+  });
+
   const handleState = () => {
     setShowPassword((showState) => {
       return !showState;
     });
   };
+
+  if (isLoggedIn) {
+    return <Redirect href={"/"} />;
+  }
   return (
-    <FormControl className="p-4 border rounded-lg max-w-[500px] border-outline-300 bg-white m-2">
+    <FormControl
+      isInvalid={loginMutation.error || sigupMutation.error}
+      className="p-4 border rounded-lg max-w-[500px] border-outline-300 bg-white m-2"
+    >
       <VStack space="xl">
         <Heading className="text-typography-900 leading-3 pt-3">Login</Heading>
         <VStack space="xs">
           <Text className="text-typography-500">Email</Text>
           <Input className="min-w-[250px]">
-            <InputField type="text" />
+            <InputField value={email} onChangeText={setEmail} type="text" />
           </Input>
         </VStack>
         <VStack space="xs">
           <Text className="text-typography-500">Password</Text>
           <Input className="text-center">
-            <InputField type={showPassword ? "text" : "password"} />
+            <InputField
+              value={password}
+              onChangeText={setPassword}
+              type={showPassword ? "text" : "password"}
+            />
             <InputSlot className="pr-3" onPress={handleState}>
               <InputIcon as={showPassword ? EyeIcon : EyeOffIcon} />
             </InputSlot>
           </Input>
         </VStack>
         <HStack space="sm">
-          <Button className="flex-1" variant="outline" onPress={() => {}}>
+          <Button
+            className="flex-1"
+            variant="outline"
+            onPress={() => sigupMutation.mutate()}
+          >
             <ButtonText>Sign up</ButtonText>
           </Button>
-          <Button className="flex-1" onPress={() => {}}>
+          <Button className="flex-1" onPress={() => loginMutation.mutate()}>
             <ButtonText>Sign in</ButtonText>
           </Button>
         </HStack>
--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -2,6 +2,7 @@ import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
 import { Icon } from "@/components/ui/icon";
 import { Text } from "@/components/ui/text";
 import "@/global.css";
+import { useAuth } from "@/store/autStore";
 import { useCart } from "@/store/cartStore";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Link, Stack } from "expo-router";
@@ -12,6 +13,8 @@ const queryClient = new QueryClient();
 
 export default function RootLayout() {
   const cartItemsNumber = useCart((state) => state.items.length);
+
+  const isLoggedIn = useAuth((state) => !!state.token);
 
   return (
     <QueryClientProvider client={queryClient}>
@@ -28,16 +31,22 @@ export default function RootLayout() {
                   </Pressable>
                 </Link>
               ),
-            headerLeft: () => (
-              <Link href={"/login"} asChild>
-                <Pressable className="flex-row gap-2">
-                  <Icon as={User} />
-                </Pressable>
-              </Link>
-            ),
           }}
         >
-          <Stack.Screen name="index" options={{ title: "Shop" }} />
+          <Stack.Screen
+            name="index"
+            options={{
+              title: "Shop",
+              headerLeft: () =>
+                !isLoggedIn && (
+                  <Link href={"/login"} asChild>
+                    <Pressable className="flex-row gap-2">
+                      <Icon as={User} />
+                    </Pressable>
+                  </Link>
+                ),
+            }}
+          />
           <Stack.Screen name="product/[id]" options={{ title: "Product" }} />
         </Stack>
       </GluestackUIProvider>
